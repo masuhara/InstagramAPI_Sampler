90c0c7c862e1f2b6f78b79f848c0129887d17000
Daisuke Masuhara <info@masuhara.net>
2014-12-28 03:21:32 +0900

    add gitignore

---
+++ InstagramAPI-Sample/APIManager.h
@@ -0,0 +1,17 @@
+//
+//  APIManager.h
+//  InstagramAPI-Sample
+//
+//  Created by Master on 2014/12/28.
+//  Copyright (c) 2014年 net.masuhara. All rights reserved.
+//
+
+#ifndef InstagramAPI_Sample_APIManager_h
+#define InstagramAPI_Sample_APIManager_h
+
+#define CLIENT_ID @"8972df7100654ab9aa245ac67231a9d6"
+#define CLIENT_SECRET @"8c2867dff81e4efd839d62fdc3b3137e"
+#define REDIRECT_URI @"scheme://api_sampler"
+
+
+#endif
